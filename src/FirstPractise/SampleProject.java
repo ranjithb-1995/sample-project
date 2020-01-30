f2adc27ab6513514f86b99eb988bb78e846fd15e
ranjithb-1995 <59864873+ranjithb-1995@users.noreply.github.com>
2020-01-30 15:38:10 +0530

    for pulling the code from github

--- src/FirstPractise/SampleProject.java
+++ src/FirstPractise/SampleProject.java
@@ -3,5 +3,6 @@ package FirstPractise;
 public class SampleProject {
 public static void main (String[] args){
 	System.out.println("first successfull commit through git and github");
+	system.out.println("checking from github to eclipse")
 }
 }
