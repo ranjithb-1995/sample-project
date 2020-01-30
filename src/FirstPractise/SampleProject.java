1cc258a5c78b74ea83221d8015041269102d089f
ranjithb-1995 <59864873+ranjithb-1995@users.noreply.github.com>
2020-01-30 16:04:08 +0530

    From github to eclipse

--- src/FirstPractise/SampleProject.java
+++ src/FirstPractise/SampleProject.java
@@ -5,5 +5,6 @@ public static void main (String[] args){
 	System.out.println("first successfull commit through git and github");
 	System.out.println("checking from github to eclipse");
 	System.out.println("from eclipse to github");
+	System.out.println("from github to eclipse");
 }
 }
