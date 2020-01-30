c8ac4776b720f6734119d9da67be912af898ccb3
Ranjith Kumar Bethini <ranjithb@achalasolutions.com>
2020-01-30 15:53:15 +0530

    single file separated

--- src/FirstPractise/SampleProject.java
+++ src/FirstPractise/SampleProject.java
@@ -3,6 +3,7 @@ package FirstPractise;
 public class SampleProject {
 public static void main (String[] args){
 	System.out.println("first successfull commit through git and github");
-	system.out.println("checking from github to eclipse")
+	System.out.println("checking from github to eclipse");
+	System.out.println("from eclipse to github");
 }
 }
